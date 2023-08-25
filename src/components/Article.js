38458d6c3f096c13b9773e8be015c1536e18777b
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-25 18:10:16 -0400

    does the bonus with a helper function!

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, time }) {
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
       <p>{preview}</p>
+      <p>{time}</p>
     </article>
   );
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -11,11 +11,34 @@ function ArticleList({ posts }) {
             title={post.title}
             date={post.date}
             preview={post.preview}
+            time={minuteRounder(post.minutes)}
           />
         );
       })}
     </main>
   );
+}
+
+function minuteRounder(minutes) {
+  let emoji;
+  let divider;
+
+  if (minutes < 30) {
+    emoji = "☕️";
+    divider = 5;
+  } else {
+    emoji = "🍱";
+    divider = 10;
+  }
+
+  const numEmoji = Math.floor(minutes / divider);
+  let emojiString = "";
+
+  for (let i = 0; i < numEmoji; i++) {
+    emojiString += emoji;
+  }
+
+  return `${emojiString} ${minutes} min read`;
 }
 
 export default ArticleList;
